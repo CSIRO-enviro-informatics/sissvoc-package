3bc8704cd33251854ef5894ec82ead891dfc82d9
Jonathan Yu <jonathan.yu@csiro.au>
2015-02-20 22:31:45 +1100

    refining the run script

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run --name "sissvoc" -P -it csiro_env/sissvoc /bin/bash
+#docker run --name "sissvoc" -P -it csiro_env/sissvoc /bin/bash
+docker run --name "sissvoc" -P -d csiro_env/sissvoc /opt/tomcat/bin/deploy-and-run.sh
